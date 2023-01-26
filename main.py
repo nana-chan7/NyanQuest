a8eeca86c91850f176d94c5b9f615143cd572f0a
{marimo_7} <{220098tt@yse-c.net}>
2023-01-26 16:44:10 +0900

    課題制作途中 アップ

---
+++ main.py
@@ -0,0 +1,178 @@
+import sys, pygame, cv2
+import random
+from pygame.locals import *
+import numpy as np
+from game import Game, Phase
+from filed import Filed
+
+# 基本処理
+pygame.init()
+clock = pygame.time.Clock()
+Game.surface = pygame.display.set_mode((Game.SCREEN_WIDTH,Game.SCREEN_HEIGHT))
+pygame.display.set_caption("***NYAN QUEST***")
+# フォント
+# font = pygame.font.Font(sys, 55)
+
+Game.field = Filed(Filed.map1,Game.surface)
+
+# ゲームの初期化処理
+def init_game_info():
+    Game.is_gameover = False
+    Game.phase = Phase.TITLE
+
+    
+# 基本描画処理   
+# def basic_draw(fill_no,img):
+#     Game.surface.fill(fill_no)
+    # if Game.phase == Phase.MAP:
+    #     # if Game.on_rightkey():
+    #     #     Game.forward_len += 5
+    #     # elif Game.on_leftkey():
+    #     #     Game.forward_len -= 5
+    #     if Game.on_rightkey():
+    #         if Game.player_pos >= 1050 or Game.r_flag:
+    #             Game.l_flag = True
+    #             Game.forward_len += 5
+    #     elif Game.on_leftkey():
+    #         if Game.player_pos <= 25 or Game.l_flag:
+    #             Game.r_flag = True
+    #             Game.forward_len -= 5
+    #     if Game.move_flag:
+    #         x = Game.forward_len % Game.SCREEN_WIDTH
+    #         Game.surface.blit(img, (-x, 0))
+    #         Game.surface.blit(img, (Game.SCREEN_WIDTH-x, 0))
+
+# ガチャ処理
+def neko_gacha():
+    # メッセージ
+    # gacha_error_msg = font.render("アイテムが足りません！また集めたら来てね！", True, (255,255,255))
+    # 確率
+    prob = [0, 0.8, 0.6, 0.5, 0.3, 0.1] 
+    # ガチャ一回につき、一体排出
+    PIC = 1
+    obtain_cara = None
+    # 所持アイテムが10以下だったらフラグをTrue
+    if Game.item < 10:
+        Game.gacha_error_flag = True
+    # 所持アイテムが10以上だったらガチャを回す
+    elif Game.item >= 10:
+        Game.gacha_error_flag = False
+        if Game.on_enterkey():
+            # アイテムを消費
+            Game.item -= 10
+            obtain_cara = random.choices(Game.CHARACTER_LIST, weights=prob, k=PIC)
+            Game.obtain_cara_img = pygame.image.load(obtain_cara[0])
+            # 手持ちに追加
+            Game.my_chara_list.append(obtain_cara[0])
+            # 所持アイテムが10以下になったらフラグをTrue
+            if Game.item < 10:
+                Game.gacha_error_flag = True   
+    # アイテムが足りなくて、回せない場合            
+    if Game.gacha_error_flag:
+        # Game.surface.blit(gacha_error_msg, [0,300]) 
+        if Game.on_return():
+            Game.phase = Phase.MAP 
+    # ガチャを回した場合、結果を表示       
+    else:
+        Game.surface.blit(Game.obtain_cara_img,  (200,200))
+        # まだアイテムが１０以上残っていたら
+        if Game.item >= 10:
+            if Game.on_0key():
+                Game.gacha_error_flag = False
+                Game.phase = Phase.GACHAGACHA
+            elif Game.on_return():
+                Game.phase = Phase.MAP
+        # アイテム数が１０以下になったら ボタン表示も変えたい
+        else:
+            # Game.surface.blit(gacha_error_msg, (200,200))        
+            if Game.on_return():
+                Game.phase = Phase.MAP     
+                  
+# メイン処理
+def main():
+    # 画像読み込み
+    title_bg = pygame.image.load("bg_images/title_img.png")
+    start_bg = pygame.image.load("bg_images/start_img.png")
+    gacha_bg = pygame.image.load("bg_images/gacha_img.png")
+    map1_bg = pygame.image.load("bg_images/map1_img.png")
+    gameover_bg = pygame.image.load("bg_images/gameover_img.png")
+    
+    # カウントダウン
+    pygame.time.set_timer(pygame.USEREVENT, 1000)
+    font = pygame.font.SysFont('Consolas', 30)
+                       
+    # ゲーム情報の初期化
+    init_game_info() 
+
+    while True:
+        Game.surface.fill((0,0,0))
+        Game.count += 1     # ゲームカウンタ
+        Game.check_event()
+        Game.move_flag = False
+    
+        # global font
+        
+        # タイトル画面
+        if Game.phase == Phase.TITLE:
+            Game.surface.blit(title_bg,(0,0))
+            if Game.on_enterkey():
+                Game.phase = Phase.START
+
+        # スタート画面
+        elif Game.phase == Phase.START:
+            Game.wait_count -= 1
+            Game.surface.blit(start_bg,(0,0))
+            # カウントダウン表示
+            if Game.wait_count % 60 == 0:
+                Game.count_down -= 1
+            Game.count_text = str(Game.count_down).rjust(3) if Game.wait_count > 0 else 'ENTER!'
+            Game.surface.blit(font.render(Game.count_text, True, (0, 0, 0)), (10, 650))
+            pygame.display.flip()
+            if Game.on_0key(): # 仮
+                Game.phase = Phase.MAP 
+                Game.move_flag = True 
+            elif Game.wait_count <= 0:
+                if Game.on_enterkey():
+                    Game.phase = Phase.MAP 
+                    Game.move_flag = True 
+                  
+        # マップ画面        
+        elif Game.phase == Phase.MAP:
+            if Game.count % 10 == 0:
+                Game.test_count += 1
+            Game.surface.fill((128,224,235))
+            # 背景のスライド    
+            x = Game.forward_len % Game.SCREEN_WIDTH
+            Game.surface.blit(map1_bg, (-x, 0))
+            Game.surface.blit(map1_bg, (Game.SCREEN_WIDTH-x, 0))
+
+            Game.field.run()  
+            if Game.on_gkey():
+                Game.phase = Phase.GACHAGACHA
+            if Game.is_gameover:
+                Game.phase = Phase.GAME_OVER
+                
+        # ガチャ画面
+        elif Game.phase == Phase.GACHAGACHA:
+            Game.surface.blit(gacha_bg,(0,0))
+            if Game.on_enterkey():
+                Game.phase = Phase.GACHARESULT
+                
+        # ガチャ結果画面
+        elif Game.phase == Phase.GACHARESULT:
+            Game.surface.fill((200,100,100))
+            neko_gacha()   
+            # video()
+   
+        # ゲームオーバー           
+        elif Game.phase == Phase.GAME_OVER:
+            Game.surface.blit(gameover_bg,(0,0))
+            if Game.on_enterkey():
+                Game.is_gameover = False
+                main()
+
+        pygame.display.update()
+        clock.tick(60)
+
+if __name__ == '__main__':
+    main()
---
+++ player.py
@@ -0,0 +1,148 @@
+import pygame
+from game import Game
+from character import Character
+
+class Player(pygame.sprite.Sprite, Character):
+    def __init__(self,pos):
+        super().__init__()
+        # 
+        # self.image = pygame.Surface((64,64))
+        # self.all_image_list = (pygame.image.load("chara_images/fue_neko.png"),
+        #                    pygame.image.load("chara_images/kotatu_neko.png"),
+        #                    pygame.image.load("chara_images/natu_neko.png"),
+        #                    pygame.image.load("chara_images/obake_neko.png"),
+        #                    pygame.image.load("chara_images/robo_neko.png"),
+        #                     pygame.image.load("chara_images/yume_neko.png"))
+        # 仮 確認用
+        self.all_image_list = (pygame.image.load("kariimage/migi1.png"),
+                           pygame.image.load("kariimage/migi2.png"),
+                           pygame.image.load("kariimage/hidari1.png"),
+                           pygame.image.load("kariimage/hidari2.png"),
+                           pygame.image.load("kariimage/jump1.png"),
+                            pygame.image.load("kariimage/jump2.png"))
+        
+        
+        self.image = self.set_chara_animation(self.all_image_list)
+        self.rect = self.image.get_rect(topleft=pos)
+        
+        # プレイヤーの動き
+        self.gravity = 0.8 #0.8
+        
+        self.screen_pos = 0
+        self.now_rect = 0
+        
+        self.player_attack_img = pygame.image.load("images/attack_star.png")
+        self.count = 0
+        
+        self.jump_coount = 0    # ジャンプカウンタ
+        self.landing = True     # 着地判定
+    
+    # キャラクターによって画像リストの差し替え
+    def change_image_list(self):
+        pass
+    # 画像は先にロードしてリストに キャラ全部のリストを作って 指定のキャラの時に呼び出し
+
+    
+    # 文字列の結合
+    # s = 'aaa' + 'bbb' + 'ccc'
+    # print(s)
+    # # aaabbbccc
+      
+    # 移動処理  
+    def get_input(self):
+        self.count += 1
+        self.now_rect = self.rect
+        Game.collided_flag = False
+        
+        # 右移動
+        if Game.on_rightkey():
+            Game.bg_stop_l = False
+            Game.direction_num = 1
+            self.rect.x += 8 
+            if self.rect.x >= Game.SCREEN_WIDTH-20:
+                self.rect.x = Game.SCREEN_WIDTH-20
+                Game.bg_stop_r = True
+            if Game.field.movement_collision():
+                self.rect.x -= 10
+            if Game.bg_stop_r:
+                Game.forward_len = 0
+            
+        # 左移動
+        if Game.on_leftkey():
+            Game.bg_stop_r = False
+            if self.rect.x <= 20:
+                self.rect.x = 20
+                Game.bg_stop_l = True
+            Game.direction_num = -1
+            self.rect.x -= 8 
+            if Game.field.movement_collision():
+                self.rect.x += 10
+            if Game.bg_stop_l:
+                Game.forward_len -= 2
+                    
+        # ジャンプ
+        if self.landing and Game.on_spacekey():  
+            self.jump()
+
+        # 重力
+        else:        
+            self.apply_gravity()
+            
+            
+        # 画面外に落下した場合ゲームオーバー
+        if self.rect.y > 704:
+            Game.is_gameover = True 
+
+    # 重力処理  
+    def apply_gravity(self):
+        for i in range(10):
+            self.rect.y += 1
+            if Game.field.movement_collision():
+                self.landing = True # 着地したら
+                self.jump_coount = 0
+                self.rect.y -= 1
+                break
+
+    # ジャンプ処理    
+    def jump(self):
+        if self.landing:
+            if self.jump_coount < 9:
+                self.rect.y -=10
+            elif self.jump_coount < 16:
+                self.rect.y -= 5
+            elif self.jump_coount < 22:
+                self.landing = False
+        if Game.field.movement_collision():
+            for i in range(10):
+                self.rect.y += 1
+            self.landing = False
+            
+        self.jump_coount += 1
+        
+    # 攻撃処理              
+    def player_attack(self):
+        Game.star_x, Game.star_y = self.rect.x, self.rect.y
+        if Game.on_skey(): 
+            Game.p_attack_flag = True
+        
+        if Game.p_attack_flag:
+            self.count += 20
+            Game.surface.blit(self.player_attack_img,(Game.star_x+self.count, Game.star_y))
+        if Game.star_x+self.count >= Game.SCREEN_WIDTH:
+            self.count = 0
+            Game.star_x, Game.star_y = self.rect.x, self.rect.y
+            Game.p_attack_flag = False
+        
+    # リスタート処理      
+    def re_start(self):
+        if Game.is_gameover:
+            self.rect.x, self.rect.y = 30, 30      
+    
+    # 更新処理            
+    def update(self):
+        self.player_attack()
+        self.get_input()
+        self.re_start() 
+        self.set_chara_animation(self.image_list)
+        
+    
