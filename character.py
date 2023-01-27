00e835aea2c43684ba0add9e399ef499280cdac8
{marimo_7} <{220098tt@yse-c.net}>
2023-01-27 14:35:40 +0900

    エネミーのアニメーション設定

--- character.py
+++ character.py
@@ -10,9 +10,8 @@ class Character:
         # 初期画像番号 0
         self.image_no = 0
         
-    # アニメーション設定  
+    # プレイヤーキャラアニメーション設定  
     def set_chara_animation(self, image_list):
-    # def set_chara_animation(self):
         self.image_list = image_list
         if Game.test_count >= 2:
             Game.test_count = 0
@@ -26,6 +25,25 @@ class Character:
             
         self.image = self.image_list[self.image_no]
         return self.image
+    
+    # エネミーキャラアニメーション設定  
+    def set_enemy_animation(self, image_list):
+        self.image_list = image_list
+        if Game.test_count >= 2:
+            Game.test_count = 0
+        self.image_no = Game.test_count
+        # if Game.on_rightkey():
+        #     self.image_no 
+        # elif Game.on_leftkey():
+        #     self.image_no += 2
+        # elif Game.on_spacekey():
+        #     self.image_no += 4
+            
+        self.image = self.image_list[self.image_no]
+        return self.image
+    
+        
+    
         
  
 
--- enemy.py
+++ enemy.py
@@ -2,27 +2,16 @@ import pygame
 from game import Game
 from character import Character
 from player import Player
-
-# class Enemy(pygame.sprite.Sprite,Character):
-#     def __init__(self,pos):
-#         super().__init__()
-#         # self.image = pygame.Surface((64,64))
-#         # self.player = Player()
-#         self.image_list = (pygame.image.load("enemy_images/nekomy1.png"),
-#                             pygame.image.load("enemy_images/nekomy2.png"))
-#         self.player_animation = Character(self.image_list,7)
-#         self.image = self.image_list[0]
-#         self.rect = self.image.get_rect(topleft=pos)
-#         self.player = Player()
         
 class Enemy(pygame.sprite.Sprite,Character):
     def __init__(self, pos, size):
         super().__init__()
-        self.image = pygame.Surface((size,size))
-        self.image_list = (pygame.image.load("enemy_images/nekomy1.png"),
-                            pygame.image.load("enemy_images/nekomy2.png"))
-        self.image = self.image_list[0]
-        # self.image.fill((100,100,100))
+        # self.image = pygame.Surface((size,size))
+        self.all_image_list = (pygame.image.load("enemy_images/nekomy1.png"),
+                            pygame.image.load("enemy_images/nekomy2.png"),
+                            pygame.image.load("enemy_images/nekomy3.png"))
+        
+        self.image = self.set_enemy_animation(self.all_image_list)
         self.rect = self.image.get_rect(topleft=pos)
         Game.enemy_x, Game.enemy_y = self.rect.x, self.rect.y
 
@@ -32,7 +21,8 @@ class Enemy(pygame.sprite.Sprite,Character):
     #     Game.is_gameover = True
         
     def update(self, x_shift):
-        self.rect.x += x_shift      
+        self.rect.x += x_shift    
+        self.set_chara_animation(self.image_list)  
     
 class Boss(pygame.sprite.Sprite,Character):
     def __init__(self,pos):
